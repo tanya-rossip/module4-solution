b19f9bf5fefddcc4fb60ba4d1b87ccf33451ed85
tanya-rossip <cascade201@gmail.com>
2022-06-27 13:18:19 +0300

    Add files via upload

---
+++ module4.js
@@ -0,0 +1,18 @@
+(function () {
+
+    const names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    
+    for (let i = 0; i < names.length; i++) {
+    
+      let firstLetter = names[i].charAt(0).toLowerCase();
+    
+      if (firstLetter === 'j') {
+        console.log('Goodbye ' + names[i]);
+      } else {
+        console.log('Hello ' + names[i]);
+      }
+    }
+    
+    })();
+
+    
